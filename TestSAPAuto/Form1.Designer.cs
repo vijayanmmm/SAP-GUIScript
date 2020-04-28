7688d90dd76a98254c69e6944782da2adeefb32b
Vijayan Venkateshan <vijayan_mmm@yahoo.com>
2020-04-28 10:02:30 +0800

    Add files via upload

---
+++ TestSAPAuto/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace TestSAPAuto {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.btnGetWinText = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.btnClickUsingUIAutomatio = new System.Windows.Forms.Button();
+            this.ctnGetLabel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "TCODE Input";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button17);
+            this.groupBox1.Controls.Add(this.button16);
+            this.groupBox1.Controls.Add(this.button15);
+            this.groupBox1.Controls.Add(this.button14);
+            this.groupBox1.Controls.Add(this.button13);
+            this.groupBox1.Controls.Add(this.btnGetWinText);
+            this.groupBox1.Controls.Add(this.button12);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.txtValue);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(67, 182);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(721, 284);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(536, 160);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(146, 50);
+            this.button16.TabIndex = 14;
+            this.button16.Text = "Select Combobox Item";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(536, 85);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(146, 50);
+            this.button15.TabIndex = 13;
+            this.button15.Text = "Click Grid Cells Select";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(536, 29);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(146, 50);
+            this.button14.TabIndex = 9;
+            this.button14.Text = "Is Window exists using UI AUtomation";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(395, 59);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(106, 37);
+            this.button13.TabIndex = 9;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // btnGetWinText
+            // 
+            this.btnGetWinText.Location = new System.Drawing.Point(288, 255);
+            this.btnGetWinText.Name = "btnGetWinText";
+            this.btnGetWinText.Size = new System.Drawing.Size(152, 23);
+            this.btnGetWinText.TabIndex = 6;
+            this.btnGetWinText.Text = "Get Window Text";
+            this.btnGetWinText.UseVisualStyleBackColor = true;
+            this.btnGetWinText.Click += new System.EventHandler(this.btnGetWinText_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(85, 255);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(152, 23);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Select Tree row";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(288, 218);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(152, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Select Menu";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(85, 218);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(152, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Select Table row";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(288, 174);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(182, 23);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "Click SAP LogOnButton";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(85, 174);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(152, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Select Context Menu";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(288, 126);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Press Button By ID";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(85, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Enter in Input Box By ID";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(134, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Enter in Input Box with EX";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Enter in Input Box";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(137, 59);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(213, 20);
+            this.txtValue.TabIndex = 3;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(137, 23);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(364, 20);
+            this.txtID.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Text box Value:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Text box ID:";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(284, 57);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(146, 51);
+            this.button11.TabIndex = 5;
+            this.button11.Text = "Save As Excel ";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // btnClickUsingUIAutomatio
+            // 
+            this.btnClickUsingUIAutomatio.Location = new System.Drawing.Point(284, 126);
+            this.btnClickUsingUIAutomatio.Name = "btnClickUsingUIAutomatio";
+            this.btnClickUsingUIAutomatio.Size = new System.Drawing.Size(146, 50);
+            this.btnClickUsingUIAutomatio.TabIndex = 6;
+            this.btnClickUsingUIAutomatio.Text = "Button  Click using UI AUtomation";
+            this.btnClickUsingUIAutomatio.UseVisualStyleBackColor = true;
+            this.btnClickUsingUIAutomatio.Click += new System.EventHandler(this.btnClickUsingUIAutomatio_Click);
+            // 
+            // ctnGetLabel
+            // 
+            this.ctnGetLabel.Location = new System.Drawing.Point(453, 57);
+            this.ctnGetLabel.Name = "ctnGetLabel";
+            this.ctnGetLabel.Size = new System.Drawing.Size(115, 51);
+            this.ctnGetLabel.TabIndex = 7;
+            this.ctnGetLabel.Text = "Get Label Text";
+            this.ctnGetLabel.UseVisualStyleBackColor = true;
+            this.ctnGetLabel.Click += new System.EventHandler(this.ctnGetLabel_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(453, 126);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(115, 51);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(536, 218);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(146, 50);
+            this.button17.TabIndex = 15;
+            this.button17.Text = "Select Combobox Item using contains";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 511);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.ctnGetLabel);
+            this.Controls.Add(this.btnClickUsingUIAutomatio);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button btnGetWinText;
+        private System.Windows.Forms.Button btnClickUsingUIAutomatio;
+        private System.Windows.Forms.Button ctnGetLabel;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+    }
+}
+
---
+++ TestSAPAuto/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SAPAuto;
+
+namespace TestSAPAuto {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.login("NRP", "100", "28066351", "Hackmeifyoucan@123", "ZH");
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.TCODEInput("ZFCN130");
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            //auto.TextInput(txtID.Text, txtValue.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            //auto.TextInput(txtID.Text, txtValue.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.TextInputByID(txtID.Text, txtValue.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.ButtonPressByID_Using_MainWindowName("客户对帐单", "wnd[2]/tbar[0]/btn[0]");
+            //auto.ButtonPressByID(txtID.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.ContextMenuSelectItem("", "Spreadsheet...");
+        }
+
+        private void button8_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.LogOnButtonClick();
+        }
+
+        private void button9_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.TableRow_Select("wnd[1]/usr/tblSAPLAQ_INT_FUNCTIONSTCH_FUNCAREAS", "6");
+        }
+
+        private void button10_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.MenuSelectionByID("wnd[0]/mbar/menu[4]/menu[8]");
+        }
+
+        private void button11_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.SaveAsExcelWindow("Customer Statement for Sales Report", @"D:\Users\28066351\Documents\Projects\RPA Metabots\SAPAuto\TestSave");
+        }
+
+        private void button12_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.TreeView_Select("wnd[0]/shellcont/shell/shellcont[0]/shell", "000006");
+        }
+
+        private void btnGetWinText_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            MessageBox.Show(auto.GetWindowText("wnd[1]"));            
+        }
+
+        private void btnClickUsingUIAutomatio_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.ButtonPress_UsingUIAutomation("SAP 轻松访问", "", "Close");
+        }
+
+        private void ctnGetLabel_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            var output =  auto.GetLabelText("作业总览", "", "", "wnd[0]/usr/lbl[64,3]");
+            MessageBox.Show("label text:" + output);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+           System.Windows.Forms.MessageBox.Show(auto.CheckAAError_IfError_Close());
+        }
+
+        private void button13_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+           // auto.testbusyornot();
+        }
+
+        private void button14_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            System.Windows.Forms.MessageBox.Show(auto.IsWindow_Exists_UsingUIAutomation_ThreeWindows("总帐科目行项目显示 总帐视图", "总帐科目 的多种选择", "总帐科目 的多种选择").ToString());
+        }
+
+        private void button15_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.Grid_SelectCell_AndDoubleClick("wnd[0]/usr/cntlGRID1/shellcont/shell", "0", "EBELN");//MESSTXT1
+        }
+
+        private void button16_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.ComboBox_SelectItem_Using_Key("wnd[0]/usr/subSUB0:SAPLMEGUI:0019/subSUB3:SAPLMEVIEWS:1100/subSUB2:SAPLMEVIEWS:1200/subSUB1:SAPLMEGUI:1301/subSUB1:SAPLMEGUI:6000/cmbDYN_6000-LIST", "   3");
+        }
+
+        private void button17_Click(object sender, EventArgs e) {
+            SAPAuto1 auto = new SAPAuto1();
+            auto.ComboBox_SelectItem_Using_ItemTextcontains("wnd[0]/usr/subSUB0:SAPLMEGUI:0019/subSUB3:SAPLMEVIEWS:1100/subSUB2:SAPLMEVIEWS:1200/subSUB1:SAPLMEGUI:1301/subSUB1:SAPLMEGUI:6000/cmbDYN_6000-LIST", "25027");
+
+        }
+    }
+}
